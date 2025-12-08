e4330f04989cb49c9cf989f878c3081f684ef038
Arunoday <arunalphap66@gmail.com>
2025-12-08 09:17:17 +0530

    Adding Input Text Fields and Programming Buttons

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from tkinter import *
 
 root = Tk()
 
-def myClick():
-    myLabel = Label(root, text='Look, Saumya I love You!')
-    myLabel.pack()
+# creating an input label
+label = Label(root, text='Enter your name? ')
+e = Entry(root, width=50)
+
+def onClick():
+    name = e.get()
+    label2 = Label(root, text=f'Your name is {name}').grid(row=3, column=1)
 
 # Creating a Button
-btn = Button(root, text='Click Me', command=myClick, fg='#4bc020', bg='black')
-btn.pack()
+btn = Button(root, text='Click Me', command=onClick)
+btn.grid(row=2, column=0)
+label.grid(row=1, column=0)
+e.grid(row=1, column=1)
 
 
 # creating the event loop
