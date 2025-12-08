b2c6185c3f22b5649acdad458fae6c69e5e3251f
Arunoday <arunalphap66@gmail.com>
2025-12-08 10:08:47 +0530

    Revert "Fully Fledged program for Saumya"
    This reverts commit e4330f04989cb49c9cf989f878c3081f684ef038.

--- app.py
+++ app.py
@@ -2,46 +2,15 @@ from tkinter import *
 
 # setting up the root and title
 root = Tk()
-root.title("Simple Calculator")
 
-# creating the entry field
-e = Entry(root, width=45, borderwidth=5)
-e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+def myClick():
+    myLabel = Label(root, text='Look, Saumya I love You!')
+    myLabel.pack()
 
-def button_show():
-    # inserting the value
-    e.insert(0, "button_value")
-    return
-    
+# Creating a Button
+btn = Button(root, text='Click Me', command=myClick, fg='#4bc020', bg='black')
+btn.pack()
 
-# creating the buttons
-numericBtns = []
-for i in range(0, 10):
-    _ = Button(root, text=f'{i}', padx=40, pady=20, command=button_show)
-    numericBtns.append(_)
 
-# placing the buttons on the screen
-rowStart = 3
-for i in range(9, 0, -3):
-    # placing the first button
-    numericBtns[i - 2].grid(row=rowStart, column=0)
-    numericBtns[i - 1].grid(row=rowStart, column=1)
-    numericBtns[i].grid(row=rowStart, column=2)
-    rowStart += 1
-
-# placing the zeroth button
-numericBtns[0].grid(row=rowStart, column=0)
-
-# creating clear screen, equals and add button
-clearSc = Button(root, text="Clear", padx=79, pady=20)
-equalBtn = Button(root, text='=', padx=91, pady=20)
-addBtn = Button(root, text='+', padx=39, pady=20)
-
-# placing the auxillary buttons
-clearSc.grid(row=rowStart, column=1, columnspan=2)
-rowStart += 1
-equalBtn.grid(row=rowStart, column=1, columnspan=2)
-addBtn.grid(row=rowStart, column=0)
-
-# firing the main loop
+# creating the event loop
 root.mainloop()
