b6cb7d083aca0b825b64e2fd79987035cc80f597
Arunoday <arunalphap66@gmail.com>
2025-12-08 09:00:10 +0530

    Creating Clickable Buttons

--- Notes.md
+++ Notes.md
@@ -14,3 +14,4 @@
     root.mainloop()
 
 Doing everything in tkinter is a two step process. You first create the widget and then place the widget on the screen.
+btn = Button(root, text='Click Me', command=myClick)
--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from tkinter import *
 
 root = Tk()
 
-# Creating a label
-myLabel = Label(root, text='Hello, World').grid(row=0, column=0)
-myLabel2 = Label(root, text='I Love You Saumya').grid(row=1, column=1)
-myLabel3 = Label(root, text='').grid(row=2, column=0)
+def myClick():
+    myLabel = Label(root, text='Look, Saumya I love You!')
+    myLabel.pack()
 
-# Positioning Using Grids
-# myLabel.grid(row=0, column=0)
-# myLabel3.grid(row=1, column=2)
-# myLabel2.grid(row=1, column=5)
+# Creating a Button
+btn = Button(root, text='Click Me', command=myClick, fg='#4bc020', bg='black')
+btn.pack()
 
 
 # creating the event loop
