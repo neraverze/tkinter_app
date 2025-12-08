d446253f73c277454f3df51b8c94701b322aa54e
Arunoday <arunalphap66@gmail.com>
2025-12-08 08:31:03 +0530

    Grid Positioning System in Tkinter

--- Notes.md
+++ Notes.md
@@ -11,4 +11,6 @@
     mylabel.pack()  # Places the label on the root screen
 
     # Creating an event loop
-    
+    root.mainloop()
+
+Doing everything in tkinter is a two step process. You first create the widget and then place the widget on the screen.
--- app.py
+++ app.py
@@ -3,8 +3,15 @@ from tkinter import *
 root = Tk()
 
 # Creating a label
-myLabel = Label(root, text='Hello, World')
-myLabel.pack()
+myLabel = Label(root, text='Hello, World').grid(row=0, column=0)
+myLabel2 = Label(root, text='I Love You Saumya').grid(row=1, column=1)
+myLabel3 = Label(root, text='').grid(row=2, column=0)
+
+# Positioning Using Grids
+# myLabel.grid(row=0, column=0)
+# myLabel3.grid(row=1, column=2)
+# myLabel2.grid(row=1, column=5)
+
 
 # creating the event loop
 root.mainloop()
