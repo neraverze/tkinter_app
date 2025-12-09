4190d9f48bcb9eb59ac11839a71d40c5c9382e06
Arunoday <arunalphap66@gmail.com>
2025-12-09 10:56:44 +0530

    Creating Window Other than Root using Toplevel()

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from tkinter import *
-from tkinter import messagebox
+from PIL import ImageTk, Image
+import os
 
 root = Tk()
 root.title("Message Boxes")
 
-# showinfo, showerror, showwarning, askokcancel, askyesno, askquestion (same as yes no)
+# getting the image address
+address = os.path.dirname(os.getcwd()) 
+files = os.listdir(address)
+my_img = ImageTk.PhotoImage(Image.open(os.path.join(address, files[0])))
 
-def popup():
-    answer = messagebox.askquestion("Info Box", "I Love You Saumya")
-    print(answer)
+def clicked():
+    # creating another window
+    top = Toplevel()
+    top.title("Saumya")
+    Label(top, image=my_img).pack()
+    Button(top, text='Quit', command=top.destroy).pack()
 
-# returns True or False depending upon the yes no clicked or ok cancel clicked
+# adding a button
+Button(root, text='Open Image', padx=40, pady=20, command=clicked).pack()
 
-Button(root, text='Popup', command=popup).pack()
-Button(root, text='Quit', command=root.quit).pack(anchor=E)
+
 
 root.mainloop()
