cd86c2ac6447826efeac476dd26842f8a916ee86
Arunoday <arunalphap66@gmail.com>
2025-12-09 11:16:06 +0530

    Using File Picker in Windows

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
 from tkinter import *
+from tkinter import filedialog
 from PIL import ImageTk, Image
 import os
 
 root = Tk()
-root.title("Message Boxes")
+root.title("File Opener")
+lbl = Label(root)
 
-# getting the image address
-address = os.path.dirname(os.getcwd()) 
-files = os.listdir(address)
-my_img = ImageTk.PhotoImage(Image.open(os.path.join(address, files[0])))
-
-def clicked():
-    # creating another window
+def openFile():
+    global my_img
+    filename = filedialog.askopenfilename(initialdir=os.path.dirname(os.getcwd()), title='Select Image File to Open', filetypes=((".jpeg Files", "*.jpg"), ("All Files", "*.*")))
+    # displaying the image in another window
     top = Toplevel()
-    top.title("Saumya")
-    Label(top, image=my_img).pack()
-    Button(top, text='Quit', command=top.destroy).pack()
+    top.title("Image Viewer")
+    
+    # opening the image
+    try:
+        originalImage = Image.open(filename)
+        originalImage.thumbnail((600, 600), resample=Image.Resampling.LANCZOS)
+        my_img = ImageTk.PhotoImage(originalImage)
+        Label(top, image=my_img).pack()
+    except Exception:
+        top.destroy()
+        lbl.config(text='Unable to Open File')
 
-# adding a button
-Button(root, text='Open Image', padx=40, pady=20, command=clicked).pack()
-
+# Displaying an Image by letting the user select an image
+openFileBtn = Button(root, text='Select Image', command=openFile)
+openFileBtn.pack()
+lbl.pack()
 
 
 root.mainloop()
